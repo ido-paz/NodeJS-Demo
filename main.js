677aed2582e6ee32a8be7ea2ae1651149bd79287
ido paz <ido_p_a@hotmail.com>
2020-06-03 13:06:45 +0300

    return Hello! in json format  from web server

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 var http = require('http');
-var message = JSON.parse('{"id":1,"text":"started server"}')
+var message = JSON.parse('{"id":1,"text":"started server","data":""}')
 //
 log(message.text);
 //
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello!');
+    log('request arrived!');
+    message.text = 'Hello!'    
+    const accpetHeader = req.headers['accept'];
+    if(accpetHeader && accpetHeader.includes('json'))  {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+    }
+    else{
+      res.writeHead(200, {'Content-Type': 'text/html'});
+    }
+    res.end(JSON.stringify(message));
   }).listen(8080);
 //
 message.text ='closed server';
 log(message.text);
 //
 function log(message){
-    console.log(new Date().getDate() + ':' + message);
+    console.log(new Date().toLocaleTimeString() + ':' + message);
 }
