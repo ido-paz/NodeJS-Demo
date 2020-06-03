143b36a01af74729363da00cbb1d748305d36ca3
ido paz <ido_p_a@hotmail.com>
2020-06-03 16:51:20 +0300

    add login , home html pages

--- README.md
+++ README.md
@@ -4,15 +4,17 @@ https://www.w3schools.com/nodejs/default.asp
 1. return hello pita string from console
 2. return Hello! in html format from web server
 3. return Hello! in json format  from web server
-4. authenticate user using basic authentication
-5. list users
-6. add user
-7. delete user
-8. working with file system
-9. working with modules
-10. hosting options for nodejs
-11. manage store data in MySQL DB
-12. unit testing code
-13. implement JWT.IO authentication 
-*from topic 2 ,return only JSON output
+4. add login , home html pages 
+5. add login , show user data controlers 
+6. authenticate user using basic authentication
+7. list users
+8. add user
+9. delete user
+10. working with file system
+11. working with modules
+12. hosting options for nodejs
+13. manage store data in MySQL DB
+14. unit testing code
+15. implement JWT.IO authentication 
+*from topic 5 ,return only JSON output
 *all technologies should be standard and most popular
--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 var message = JSON.parse('{"id":1,"text":"started server","data":""}')
 //
 log(message.text);
 //
 http.createServer(function (req, res) {
-    log('request arrived!');
-    message.text = 'Hello!'    
-    const accpetHeader = req.headers['accept'];
-    if(accpetHeader && accpetHeader.includes('json'))  {
-      res.writeHead(200, {'Content-Type': 'application/json'});
+    log('started to handle request!');
+    //
+    if (req.url.includes('html')) {
+      handleFileRequest(req,res);
     }
     else{
-      res.writeHead(200, {'Content-Type': 'text/html'});
+      message.text = 'Hello!'    
+      const accpetHeader = req.headers['accept'];
+      if(accpetHeader && accpetHeader.includes('json'))  {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+      }
+      else{
+        res.writeHead(200, {'Content-Type': 'text/html'});
+      }
+      res.end(JSON.stringify(message));
     }
-    res.end(JSON.stringify(message));
+    //
+    log('finished to handle request');
   }).listen(8080);
 //
 message.text ='closed server';
 log(message.text);
+//
+function handleFileRequest(req,res){
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}
 //
 function log(message){
     console.log(new Date().toLocaleTimeString() + ':' + message);
